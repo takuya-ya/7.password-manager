802c906becc45a6c2e68295619c69b2d329ec541
takuya <cdwbh564@yahoo.co.jp>
2024-09-20 22:25:10 +0900

    ユーザー入力を受け取る処理を関数化

--- password_manager.sh
+++ password_manager.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+password_manager_input()
+{
+#入力を受け取る
+echo 'パスワードマネージャーへようこそ!'
+echo 'サービス名を入力して下さい : '
+read service
+echo 'ユーザー名を入力してください : '
+read user
+echo 'パスワードを入力してください : '
+read pass
+#色を変更
+printf 'Thank you\033[31m!\033[0m\n'
+}
+
+password_manager_input
+
+
+
+
+
+# #バリデーション処理
+# save_login_data
+# #入力をファイルへリダイレクトして保存
+# echo ${service}:${user}:${pass} >> login_data.text
+# #エラー処理
+
 << COMMENTOUT
 ステップ1
 パスワードを保存する処理を、シェルスクリプトで作成します。
@@ -18,18 +45,3 @@
 Thank you!
 サービス名、ユーザー名、パスワードの入力が求められ、入力された情報を "サービス名:ユーザー名:パスワード" という形式でファイルに保存します。シェルスクリプトを実行するたびに入力された情報はファイルに追記されるようにしてください。
 COMMENTOUT
-
-#入力を受け取る
-echo 'パスワードマネージャーへようこそ!'
-echo 'サービス名を入力して下さい : '
-read service
-echo 'ユーザー名を入力してください : '
-read user
-echo 'パスワードを入力してください : '
-read pass
-printf 'Thank you\033[31m!\033[0m'
-
-#色を変更
-#バリデーション処理
-#データをリダイレクト
-#エラー処理
