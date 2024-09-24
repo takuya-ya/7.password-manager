be0ecf4da836f04a511ff266e4c6c0bce407c138
takuya <cdwbh564@yahoo.co.jp>
2024-09-24 22:15:06 +0900

    メニューを表示する処理を追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+display_menu() {
+    while true; do
+        echo "パスワードマネージャーへようこそ！"
+        echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+        read menu
+        if [ "${menu}" = "Add Password" ]; then
+            addassword
+        elif [  "${menu}" = "Get Password" ]; then
+            GetPassword
+        elif [ "${menu}" = "Exit" ]; then
+            exit
+        else
+            echo '選択肢から入力してください'
+            echo
+        fi
+    done
+}
+
 password_manager_input()
 {
     echo 'パスワードマネージャーへようこそ!'
@@ -43,32 +61,55 @@ save_login_data() {
     echo "${service_name}:${user_name}:${password}" >> keep_login_data.txt
 }
 
+display_menu
 password_manager_input
 validate_input
 display_errors
 
 if [ -z "${error_status}" ]; then
-    save_login_data
+    # save_login_data
     printf 'Thank you\033[31m!\033[0m\n'
 fi
-# #エラー処理
 
 << COMMENTOUT
-ステップ1
-パスワードを保存する処理を、シェルスクリプトで作成します。
 
-以下の情報を保存できるようにします。この情報はファイルに保存してください。
+繰返し
+選択肢表示して入力を求める
+    選択肢以外は無効
+入力に応じて選択し表示
 
-サービス名
-ユーザー名
-パスワード
-シェルスクリプトを実行すると、以下が表示されます。
+選択肢の関数作成
+テスト
 
-パスワードマネージャーへようこそ！
+Add Password が入力されると、サービス名、ユーザー名、パスワードの入力が求められ、入力された情報をファイルに保存します。
+ Get Password が入力されると、サービス名の入力が求められ、入力されたサービスのサービス名、ユーザー名、パスワードが表示されます。
+ Exit が入力されると、プログラムが終了します。 Exit が入力されるまではプログラムは終了せず、「次の選択肢から入力してください(Add Password/Get Password/Exit)：」が繰り返されます。
+
+▼アウトプット
+
+# Add Password が入力された場合
 サービス名を入力してください：
 ユーザー名を入力してください：
 パスワードを入力してください：
-# 入力が完了したら
+
+パスワードの追加は成功しました。
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Get Password が入力された場合
+サービス名を入力してください：
+## サービス名が保存されていなかった場合
+そのサービスは登録されていません。
+## サービス名が保存されていた場合
+サービス名：hoge
+ユーザー名：fuga
+パスワード：piyo
+
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Exit が入力された場合
 Thank you!
-サービス名、ユーザー名、パスワードの入力が求められ、入力された情報を "サービス名:ユーザー名:パスワード" という形式でファイルに保存します。シェルスクリプトを実行するたびに入力された情報はファイルに追記されるようにしてください。
+## プログラムが終了
+
+# Add Password/Get Password/Exit 以外が入力された場合
+入力が間違えています。Add Password/Get Password/Exit から入力してください。
 COMMENTOUT
